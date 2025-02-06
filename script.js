71265bd86f756b41c536e28e69ab6ebcd260fd3f
blacknblue <138015901+personalityblu@users.noreply.github.com>
2025-02-06 21:43:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function checkPin() {
+    const pinInput = document.getElementById('pinInput').value;
+    const message = document.getElementById('message');
+
+    if (pinInput === "0205") {
+        message.textContent = "Access Granted!";
+        message.style.color = "green";
+        window.location.href = "index.html";
+    } else {
+        message.textContent = "Access Denied! Try again.";
+        message.style.color = "red";
+    }
+}
+
+function hideButton() {
+  const button = document.querySelector('.btn_div');
+  const letter = document.querySelector('.letterdiv');
+  const lett = document.querySelector('.letter');
+  const surprise = document.querySelector('.btn_surprise');
+  const audio = document.getElementById('aud');
+  button.style.display = 'none';
+  letter.style.display = 'block';
+  const text = lett.innerHTML; // Get the text content
+  lett.innerHTML = ''; // Clear the paragraph content
+
+  let index = 0; // Initialize index for characters
+  const speed = 50; // Speed of typing in milliseconds
+
+  audio.play();
+
+  function type() {
+    if (index < text.length) {
+      lett.innerHTML += text.charAt(index); // Add one character
+      index++;
+      setTimeout(type, speed); // Call type function again after a delay
+    } else {
+        surprise.style.display = 'block';
+    }
+  }
+
+  type(); // Start the typing effect
+}
+
+function surprise() {
+  const letter = document.querySelector('.letterdiv');
+  const surp = document.querySelector('.flowers_div');
+  letter.style.display = 'none';
+  surp.style.display = 'inline-block';
+}
+
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
